937e38725e05574d19535392f67bf2469469b0f5
Raymond Buckley <raymond.ray3k@gmail.com>
2017-04-26 20:14:33 -0700

    Initial project setup.

---
+++ desktop/src/com/ray3k/bobby/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ray3k.bobby.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ray3k.bobby.Core;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Core(), config);
+	}
+}
